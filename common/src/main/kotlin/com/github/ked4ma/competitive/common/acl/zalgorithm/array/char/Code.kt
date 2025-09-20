6c88bd378f3ecde0d5f419579931da02101d2b53
ked4ma <hades.netherworld@gmail.com>
2025-09-20 19:17:10 +0900

    add z-algorithm

---
+++ common/src/main/kotlin/com/github/ked4ma/competitive/common/acl/zalgorithm/array/char/Code.kt
@@ -0,0 +1,23 @@
+package com.github.ked4ma.competitive.common.acl.zalgorithm.array.char
+
+import com.github.ked4ma.competitive.common.array.int.d1.*
+import kotlin.math.min
+
+// https://github.com/atcoder/ac-library/blob/master/atcoder/string.hpp#L247-L273
+fun CharArray.zAlgorithm(): IntArray {
+    val n = this.size
+    if (n == 0) return IntArray(0)
+    val z = sizedIntArray(n + 1, 0)
+    z[0] = 0;
+    var i = 1
+    var j = 0
+    while (i < n) {
+        var k = if (j + z[j] <= i) 0 else min(j + z[j] - i, z[i - j])
+        while (i + k < n && this[k] == this[i + k]) k++
+        z[i] = k
+        if (j + z[j] < i + z[i]) j = i
+        i++
+    }
+    z[0] = n;
+    return z;
+}
